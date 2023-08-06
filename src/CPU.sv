3344426c066898250b527b0ef639ae3779633a9b
HUAIJIE0314 <60058096+HUAIJIE0314@users.noreply.github.com>
2023-08-07 04:03:11 +0800

    Add files via upload

--- src/CPU.sv
+++ src/CPU.sv
@@ -160,8 +160,8 @@ assign opcode_ID   = Inst_ID[`OP_RANGE    ];
 assign CSRSel_ID   = {Inst_ID[21], Inst_ID[27]};
 
 // << branch target >>
-assign PC_plus4_IF       = PC_pres_IF_tmp + 32'd4;
-assign branchTarget      = rs1orFWorPC + imm_out_ID;
+assign PC_plus4_IF = PC_pres_IF_tmp + 32'd4;
+assign branchTarge = rs1orFWorPC + imm_out_ID;
 
 // << branch_taken >>
 assign branch_taken = (Branch_ID&branch_flag_ID)|Jal_ID|Jalr_ID;
